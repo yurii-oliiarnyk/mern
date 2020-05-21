1b34e3d131822335d02519f9289c322bdf3d3c98
yurii-oliiarnyk <oliiarnyk14@gmail.com>
2020-05-21 23:42:50 +0300

    Connect backend to frontend static

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = config.get("port") || 5000;
 const URI = config.get("mongoUri");
@@ -11,6 +12,14 @@ app.use(express.json({ extened: true }));
 app.use("/api/auth", require("./routes/auth-routes"));
 app.use("/api/links", require("./routes/links-routes"));
 app.use("/t", require("./routes/redirect-routes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 async function start() {
   try {
